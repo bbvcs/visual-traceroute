15080c10df20ee4d338f35e47832ac92fa30ae3c
Billy C Smith <bcsm@posteo.net>
2021-07-28 14:26:13 +0100

    put the getting of ipinfo (via pycurl) into seperate func, and added json library to format response into python dict

--- ip_helper.py
+++ ip_helper.py
@@ -1,6 +1,7 @@
 import socket 			# for converting hostname to IP
 import pycurl 			# for getting info about IP via ipinfo.io
 import certifi 			# for HTTPS security with PycURL
+import json				# for converting ipinfo.io result (json) to py dict
 
 from io import BytesIO 	# for buffer for pycurl to write into
 
@@ -8,17 +9,13 @@ from io import BytesIO 	# for buffer for pycurl to write into
 def format_hostname(hostname): # UNIMPLEMENTED
 	"""Format the hostname to only the host, 
 	e.g www.website.com/page12/option7 to www.website.com"""
+	
 	return hostname 
-	
 
-if __name__ == "__main__":
+def get_ipinfo_dict(ip_addr):
+	"""Takes an IPv4 address and uses ipinfo.io to get information in
+	json format, and convert to python dictionary"""
 	
-	hostname = "www.google.com" # taken in as param probably
-	hostname = format_hostname(hostname)
-	# CHECK IF HOSTNAME OR IP
-	ip_addr  = socket.gethostbyname(hostname)
-	
-
 	buffer = BytesIO()
 	c = pycurl.Curl()
 	c.setopt(c.URL, "https://ipinfo.io/"+ip_addr+"/json")
@@ -28,4 +25,20 @@ if __name__ == "__main__":
 	c.close
 
 	body = buffer.getvalue()
-	print(body.decode('iso-8859-1'))
+	json_format_str = body.decode('iso-8859-1')
+	return json.loads(json_format_str) # json.loads returns python dict
+	
+
+
+if __name__ == "__main__":
+	
+	hostname = "www.google.com" 
+	hostname = format_hostname(hostname)
+	# CHECK IF HOSTNAME OR IP
+	ip_addr  = socket.gethostbyname(hostname)
+	
+	print(type(get_ipinfo_dict(ip_addr)))
+	
+
+	
+	
